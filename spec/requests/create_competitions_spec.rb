de9696832bba297b16a024abf7204759535725f1
Nate <namoricoo@hotmail.com>
2014-02-16 15:44:36 -0800

    empty competition file before we add first test

--- spec/requests/create_competitions_spec.rb
+++ spec/requests/create_competitions_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 describe "CreateCompetitions" do
-  describe "GET /create_competitions" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get create_competitions_path
-      response.status.should be(200)
-    end
+  it 'creates competitions' do
+    
   end
 end
