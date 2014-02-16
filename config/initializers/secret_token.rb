047dfda831f2f65546f50887ab5028c8e392c729
Nate <namoricoo@hotmail.com>
2014-02-15 22:06:02 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeoPictures::Application.config.secret_key_base = '666d446cb2122f13065fb3bfedeae810318fdc6c5f9284366c64e1cb0eaa11d25eec06e222155d24b12cfae928e86c45028db63517f2c23ac6a251fe6811c9c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GeoPictures::Application.config.session_store :cookie_store, key: '_geo_pictures_session'
