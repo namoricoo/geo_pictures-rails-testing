8777f29df2fe31fe55acea7a14e9a7484d9d5f1f
Nate <namoricoo@hotmail.com>
2014-02-16 15:01:20 -0800

    create main_page controller with home view

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,13 +1,17 @@
 class Location < ActiveRecord::Base
+  validates :latitude, :presence => true
+
   # attr_accessible :latitude, :longitude
   R = 3_959 # Earth's radius in mi, approx def near?( lat, long, mile_radius)
   def near?( lat, long, mile_radius)
     # omitted
   end
+  
   private 
   def to_radians( degrees)
     # omitted
   end
+  
   def haversine_distance( loc)
     # omitted
   end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,19 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Location do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  
+  # describe "validations" do
+    # before { subject.valid? }
+    # [ :latitude ]. each do | coordinate |
+       # context "when #{ coordinate} is nil" do
+          # subject { Location.new( coordinate => nil) } 
+          # it "shouldn't allow blank #{ coordinate}" do
+            # expect( subject.errors_on( coordinate))
+            # .to include(" can't be blank")
+          # end
+      # end
+    # end
+  # end
+     
 end
