b200ae4956530eccf7df3fa002e982e0a43166f8
Nate <namoricoo@hotmail.com>
2014-02-16 11:12:25 -0800

    add location active record function stubs

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,18 @@
 class Location < ActiveRecord::Base
+  # attr_accessible :latitude, :longitude
+  R = 3_959 # Earth's radius in mi, approx def near?( lat, long, mile_radius)
+  def near?( lat, long, mile_radius)
+    # omitted
+  end
+  private 
+  def to_radians( degrees)
+    # omitted
+  end
+  def haversine_distance( loc)
+    # omitted
+  end
+  
+  def app_params
+    params.require(:location).permit(:latitude,:longitude)
+  end
 end
