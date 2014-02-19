d58466ee61ac8edb864bb0b43c18de031f8debb1
Nate <namoricoo@hotmail.com>
2014-02-18 23:27:32 -0800

    save competition controller updates to github

--- spec/features/create_competitions_spec.rb
+++ spec/features/create_competitions_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "CreateCompetitions" do
   it 'creates competitions' do
-    visit new_competition_path    
+    visit new_competition_path
     fill_in "Name",with: "Random"
     click_button "Submit"
   end
